2628ee46308b99d3de3364cbecb7ab89532145a9
KSait0 <6wayofpain@gmail.com>
2023-08-16 19:40:07 +0300

    Выполнил 1.17 Начинаем программировать

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+import { dirname, resolve } from 'path';
+import CopyPlugin from 'copy-webpack-plugin';
+import HtmlPlugin from 'html-webpack-plugin';
+import { fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public',
+      globOptions: {
+        ignore: ['**/index.html'],
+      },
+    }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ]
+  }
+};
